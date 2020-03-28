355d4e0d8eaf4673c56bcd12a702baeb0ae44f29
Saurabh Daware <saurabhdaware99@gmail.com>
2020-03-28 17:13:23 +0530

    circular distortion addd

--- js/app.js
+++ js/app.js
@@ -4,6 +4,76 @@ const pageContainerEl = document.querySelector('.page');
 const textareaEl = document.querySelector('.page > .textarea');
 const overlayEl = document.querySelector('.page > .overlay');
 
+
+
+function distortion(outputCanvas) {
+  const ctx = outputCanvas.getContext('2d');
+  const distortWidth = outputCanvas.width;
+  const distortHeight = outputCanvas.height*1.3;
+  const x = 0;
+  const y = -100;
+
+  var imgData = ctx.getImageData(x, y, distortWidth, distortHeight);
+      pixels = imgData.data,
+      pixelsCopy = [], index = 0, h = distortHeight, w = distortWidth;
+  
+  for (var i = 0; i <= pixels.length; i+=4) {
+      pixelsCopy[index] = [pixels[i], pixels[i+1], pixels[i+2], pixels[i+3]];
+      index++;
+  }
+  console.log(pixelsCopy);
+  var result = fisheye(pixelsCopy, w, h);
+  
+  for(var i = 0; i < result.length; i++) {
+      index = 4*i;
+      if (result[i] != undefined) {
+          pixels[index + 0] = result[i][0];
+          pixels[index + 1] = result[i][1];
+          pixels[index + 2] = result[i][2]; 
+          pixels[index + 3] = result[i][3];
+      }
+  }
+  
+  ctx.putImageData(imgData, x, y);
+}
+
+function fisheye(srcpixels, w, h) {
+
+  var dstpixels = srcpixels.slice();            
+  for (var y = 0; y < h; y++) {                                
+
+      var ny = ((2*y)/h)-1;                        
+      var ny2 = ny*ny;                                
+
+      for (var x = 0; x < w; x++) {                            
+
+          var nx = ((2*x)/w)-1;                    
+          var nx2 = nx*nx;
+          var r = Math.sqrt(nx2+ny2); // Calculates radius        
+
+          if (0.0 <= r && r <= 1.0) {                            
+              var nr = Math.sqrt(1.0-r*r);            
+              nr = (r + (1.0-nr)) / 2.0;
+
+              if (nr <= 1.0) {
+                  
+                  var theta = Math.atan2(ny,nx);         
+                  var nxn = nr*Math.cos(theta);        
+                  var nyn = nr*Math.sin(theta);        
+                  var x2 = parseInt(((nxn+1)*w)/2);        
+                  var y2 = parseInt(((nyn+1)*h)/2);        
+                  var srcpos = parseInt(y2*w+x2);            
+                  if (srcpos >= 0 & srcpos < w*h) {
+                      dstpixels[parseInt(y*w+x)] = srcpixels[srcpos];    
+                  }
+              }
+          }
+      }
+  }
+  return dstpixels;
+} 
+
+
 function readFile(fileObj) {
   const reader = new FileReader();
   reader.onload = e => {
@@ -45,6 +115,8 @@ async function generateImage() {
         scrollX: 0,
         scrollY: -window.scrollY
       })
+
+    distortion(canvas);
     
     document.querySelector('.output').innerHTML = '';
     const img = document.createElement('img');
