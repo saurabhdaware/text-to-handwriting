e8db4a726207aff5e64bbc7cffc8c80605f5f2d4
anuraghazra <hazru.anurag@gmail.com>
2020-04-12 17:07:07 +0530

    refactor: refactored events

--- js/app.mjs
+++ js/app.mjs
@@ -1,23 +1,20 @@
 import {
+  $,
   isMobile,
   applyPaperStyles,
   removePaperStyles,
-  addFontFromFile
-} from './helpers.mjs'
+  addFontFromFile,
+  addEvent,
+} from './helpers.mjs';
+import { setInkColor, toggleDrawCanvas } from './draw.mjs';
 
-import {
-  setInkColor, 
-  toggleDrawCanvas
-} from './draw.mjs';
-
-const textareaEl = document.querySelector('.page > .textarea');
-
+const textareaEl = $('.page > .textarea');
 
 /**
  * @method generateImage()
  * @description
  *    1. Apply CSS Styles that make text field look like paper
- *    2. Use html2canvas library to turn the HTML DOM to Canvas 
+ *    2. Use html2canvas library to turn the HTML DOM to Canvas
  *    3. Get image out of canvas and render new <img> tag
  *    4. Enable download image buttons
  *    5. Remove the previously applied CSS Styles.
@@ -26,114 +23,115 @@ async function generateImage() {
   // apply extra styles to textarea to make it look like paper
   applyPaperStyles();
 
-  try{
-    const canvas = await html2canvas(document.querySelector(".page"), {
-        scrollX: 0,
-        scrollY: -window.scrollY
-      })
-    
-    document.querySelector('.output').innerHTML = '';
-    const img = document.createElement('img');
-    img.src = canvas.toDataURL("image/jpeg");
-    document.querySelector('.output').appendChild(img);
+  try {
+    const canvas = await html2canvas($('.page'), {
+      scrollX: 0,
+      scrollY: -window.scrollY,
+    });
 
-    document.querySelectorAll('a.download-button').forEach(a => {
+    const output = $('.output');
+    const img = new Image();
+    img.src = canvas.toDataURL('image/jpeg');
+    output.innerHTML = '';
+    output.appendChild(img);
+
+    document.querySelectorAll('a.download-button').forEach((a) => {
       a.href = img.src;
       a.download = 'assignment';
       a.classList.remove('disabled');
-    })
-  }catch(err) {
-    alert("Something went wrong :(");
+    });
+  } catch (err) {
+    alert('Something went wrong :(');
     console.error(err);
   }
 
   // Now remove styles to get textarea back to normal
   removePaperStyles();
 
-  if(isMobile) {
+  if (isMobile) {
     smoothlyScrollTo('#output');
   }
 }
 
 // Convert copied text to plaintext
-document.querySelector("#note").addEventListener('paste', (event) => {
-  if(!event.clipboardData.types.includes('Files')) {
+addEvent('#note', 'paste', (event) => {
+  if (!event.clipboardData.types.includes('Files')) {
     event.preventDefault();
-    var text = event.clipboardData.getData("text/plain");
-    document.execCommand("insertHTML", false, text);
+    const text = event.clipboardData.getData('text/plain');
+    document.execCommand('insertHTML', false, text);
   }
-})
-
+});
 
 /**
  * Event listeners on input fields
  */
+const ID = {
+  HANDWRITING: '#handwriting-font',
+  PEN_COLOR: '#ink-color',
+  FONT_SIZE: '#font-size',
+  TOP_PADDING: '#top-padding',
+  WORD_SPACING: '#word-spacing',
+  FONT_FILE: '#font-file',
+  TOGGLE_MARGIN: '#paper-margin-toggle',
+};
 
-document.querySelector('select#handwriting-font')
-  .addEventListener('change', e => textareaEl.style.fontFamily = e.target.value)
+/**
+ *
+ * @param {string} attrib
+ * @param {any} value
+ */
+const setStyle = (attrib, v) => {
+  textareaEl.style[attrib] = v;
+};
 
-document.querySelector('select#ink-color')
-  .addEventListener('change', e => {
-    textareaEl.style.color = e.target.value;
-    setInkColor(e.target.value);
-  })
+/**
+ *
+ * @param {ChangeEvent} e
+ */
+const handleToolEvents = (e) => {
+  e.preventDefault();
 
-document.querySelector('input#font-size')
-  .addEventListener('change', e => textareaEl.style.fontSize = e.target.value + 'pt')
+  const id = '#' + e.target.getAttribute('id');
+  switch (id) {
+    case ID.HANDWRITING:
+      setStyle('fontFamily', e.target.value);
+      break;
+    case ID.FONT_SIZE:
+      setStyle('fontSize', e.target.value + 'pt');
+      break;
+    case ID.TOP_PADDING:
+      setStyle('paddingTop', e.target.value + 'px');
+      break;
+    case ID.WORD_SPACING:
+      setStyle('wordSpacing', e.target.value + 'px');
+      break;
+    case ID.FONT_FILE:
+      addFontFromFile(e.target.files[0]);
+      break;
+    case ID.TOGGLE_MARGIN:
+      $('.page').classList.toggle('margined-page');
+      break;
+    case ID.PEN_COLOR:
+      setStyle('color', e.target.value);
+      setInkColor(e.target.value);
+      break;
+    default:
+      return;
+  }
+};
 
-document.querySelector('input#top-padding')
-  .addEventListener('change', e => textareaEl.style.paddingTop = e.target.value + 'px')
+// form change events
+document
+  .querySelector('#generate-image-form')
+  .addEventListener('change', handleToolEvents);
 
-document.querySelector('input#word-spacing')
-  .addEventListener('change', e => textareaEl.style.wordSpacing = e.target.value + 'px')
-
-document.querySelector('#font-file')
-  .addEventListener('change', e => {
-    addFontFromFile(e.target.files[0])
-  })
-
-document.querySelector('#paper-margin-toggle')
-  .addEventListener('change', e => 
-    document.querySelector('.page').classList.toggle('margined-page')
-  )
-
-document.querySelector('button#draw-diagram-button')
-  .addEventListener('click', toggleDrawCanvas)
-
-document.querySelector('.draw-container .close-button')
-  .addEventListener('click', toggleDrawCanvas)
-
-// Generate image on form submit
-document.querySelector('form#generate-image-form')
-  .addEventListener('submit', e => {
-    e.preventDefault(); // prevents reloading
-    generateImage();
-  })
-
-
-/***
- * Mostly the code nobody has to care about
-*/
-
-function smoothlyScrollTo(hashval) {
-  let target = document.querySelector(hashval)
-  target.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  })
-  history.pushState(null, null, hashval)
-}
-
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]')
-  .forEach(item => {
-    item.addEventListener('click', (e)=> {
-      let hashval = item.getAttribute('href')
-      smoothlyScrollTo(hashval);
-      e.preventDefault()
-    })
-  })
-
+// click events
+addEvent('#draw-diagram-button', 'click', toggleDrawCanvas);
+addEvent('.draw-container .close-button', 'click', toggleDrawCanvas);
+addEvent('#generate-image-form', 'submit', (e) => {
+  e.preventDefault();
+  generateImage();
+});
 
 // Too lazy to change year in footer every year soo...
-document.querySelector('#year').innerHTML = new Date().getFullYear();
+$('#year').innerHTML = new Date().getFullYear();
--- js/helpers.mjs
+++ js/helpers.mjs
@@ -1,43 +1,63 @@
-
 const pageContainerEl = document.querySelector('.page');
 const textareaEl = document.querySelector('.page > .textarea');
 const overlayEl = document.querySelector('.page > .overlay');
 
+export const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-function applyPaperStyles() {
+export const applyPaperStyles = () => {
   pageContainerEl.style.border = 'none';
   pageContainerEl.style.background = 'linear-gradient(to right,#eee, #ddd)';
-  overlayEl.style.background = `linear-gradient(${Math.random()*360}deg, #0008, #0000)`
+  overlayEl.style.background = `linear-gradient(${
+    Math.random() * 360
+  }deg, #0008, #0000)`;
   overlayEl.style.display = 'block';
   textareaEl.classList.add('paper');
-}
+};
 
-function removePaperStyles() {
+export const removePaperStyles = () => {
   pageContainerEl.style.border = '1px solid #ccc';
   pageContainerEl.style.background = 'linear-gradient(to right,#fff, #fff)';
   overlayEl.style.display = 'none';
   textareaEl.classList.remove('paper');
-}
+};
 
-function addFontFromFile(fileObj) {
+export const addFontFromFile = (fileObj) => {
   const reader = new FileReader();
-  reader.onload = e => {
+  reader.onload = (e) => {
     const newFont = new FontFace('temp-font', e.target.result);
-    newFont.load()
-      .then(loadedFace => {
-        document.fonts.add(loadedFace);
-        textareaEl.style.fontFamily = 'temp-font';
-      })
-  }
-  reader.readAsArrayBuffer(fileObj)
-}
+    newFont.load().then((loadedFace) => {
+      document.fonts.add(loadedFace);
+      textareaEl.style.fontFamily = 'temp-font';
+    });
+  };
+  reader.readAsArrayBuffer(fileObj);
+};
 
+/**
+ *
+ * @param {string} hashval
+ */
+export const smoothlyScrollTo = (hashval) => {
+  let target = document.querySelector(hashval);
+  target.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+  history.pushState(null, null, hashval);
+};
 
-export {
-  isMobile,
-  applyPaperStyles,
-  removePaperStyles,
-  addFontFromFile
-}
+/**
+ *
+ * @param {string} _
+ */
+export const $ = (_) => document.querySelector(_);
+
+/**
+ *
+ * @param {string} selector
+ * @param {string} eventName
+ * @param {function} callback
+ */
+export const addEvent = (selector, eventName, callback) => {
+  document.querySelector(selector).addEventListener(eventName, callback);
+};
