ab992a76e5a9cc7b2f81e9703dc33e5ad84cd771
Saurabh Daware <saurabhdaware99@gmail.com>
2020-04-24 23:02:59 +0530

    dynamic alias fix for jspdf addImage

--- js/pdf.mjs
+++ js/pdf.mjs
@@ -3,9 +3,9 @@ function createPDF(imgs) {
     var doc = new jsPDF('p', 'pt', 'a4');
     var width = doc.internal.pageSize.width;
     var height = doc.internal.pageSize.height;
-    for (var i = 0; i < imgs.length; i++) {
+    for (let i in imgs) {
         doc.text(10, 20, 'Scanned by Anonymous Scanner');
-        doc.addImage(imgs[i], 'JPEG', 25, 50, width - 50, (height - 80), 36);
+        doc.addImage(imgs[i], 'JPEG', 25, 50, width - 50, (height - 80), "image-"+i);
         if (i != imgs.length - 1) {
             doc.addPage();
         }
