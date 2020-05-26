9da4f8d76ef45bdbc9082ca3026daffa3f0aac88
Saurabh Daware <saurabhdaware99@gmail.com>
2020-05-27 00:13:25 +0530

    removed scanned by anoynmouse software text

--- js/helpers.mjs
+++ js/helpers.mjs
@@ -45,7 +45,7 @@ function createPDF(imgs) {
   const width = doc.internal.pageSize.width;
   const height = doc.internal.pageSize.height;
   for (let i in imgs) {
-    doc.text(10, 20, 'Scanned by Anonymous Scanner');
+    doc.text(10, 20, '');
     doc.addImage(imgs[i], 'JPEG', 25, 50, width - 50, (height - 80), "image-"+i);
     if (i != imgs.length - 1) {
       doc.addPage();
