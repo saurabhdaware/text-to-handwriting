6f44e3ff4e44ec27707d4b7eb879112910ec842c
Saurabh Daware <saurabhdaware99@gmail.com>
2020-06-26 13:58:44 +0530

    basic output generation

--- js/generate-images.mjs
+++ js/generate-images.mjs
@@ -1,3 +1,40 @@
-export function generateImages() {
-  console.log("Generating Images...")
+import {
+  applyPaperStyles,
+  removePaperStyles
+} from './utils/generate-utils.mjs'
+
+const pageEl = document.querySelector('.page-a');
+const outputImages = [];
+
+function renderOutput() {
+  if (outputImages.length <= 0) {
+    document.querySelector('#output').innerHTML = "Click \"Generate Image\" Button to generate new image.";
+    return;
+  }
+  
+  document.querySelector('#output').innerHTML = outputImages
+    .map((outputImageCanvas, index) => `
+      <img 
+        class="shadow" 
+        alt="Output image ${index}" 
+        src="${outputImageCanvas.toDataURL('image/jpeg')}"
+      />
+    `)
+    .join('');
+}
+
+export async function generateImages() {
+  applyPaperStyles();
+
+  const canvas = await html2canvas(pageEl, {
+    scrollX: 0,
+    scrollY: -window.scrollY,
+    scale: 1.5
+  });
+
+  outputImages.push(canvas);
+
+  removePaperStyles();
+
+  renderOutput();
 }
--- js/utils/generate-utils.mjs
+++ js/utils/generate-utils.mjs
@@ -1,19 +1,28 @@
-function removePaperStyles() {
-  pageContainerEl.style.border = '1px solid #ccc';
-  pageContainerEl.style.background = 'linear-gradient(to right,#fff, #fff)';
-  overlayEl.style.display = 'none';
-  textareaEl.classList.remove('paper');
-}
+const pageEl = document.querySelector('.page-a');
+const paperContentEl = document.querySelector('.page-a .paper-content');
+let paperContentPadding;
 
+function isFontErrory() {
+  // SOme fonts have padding top errors, this functions tells you if the current font has that;
+  const currentHandwritingFont = document.body.style.getPropertyValue('--handwriting-font');
+  return currentHandwritingFont === '' || currentHandwritingFont.includes('Homemade Apple');
+}
 
 function applyPaperStyles() {
-  pageContainerEl.style.border = 'none';
-  overlayEl.style.background = `linear-gradient(${
-    Math.random() * 360
-  }deg, #0008, #0000)`;
-  overlayEl.style.display = 'block';
+  pageEl.style.border = 'none';
+  if (isFontErrory()) {
+    paperContentPadding = paperContentEl.style.paddingTop.replace(/px/g, '') || 5;
+    let newPadding = Number(paperContentPadding) - 5;
+    paperContentEl.style.paddingTop = `${newPadding}px`;
+  }
 }
 
+function removePaperStyles() {
+  pageEl.style.border = '1px solid var(--elevation-background)';
+  if (isFontErrory()) {
+    paperContentEl.style.paddingTop = `${paperContentPadding}px`;
+  }
+}
 export {
   removePaperStyles,
   applyPaperStyles
